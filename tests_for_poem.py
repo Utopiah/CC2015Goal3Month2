6b12a073be79991d2b6596745e64f9d49a6dba05
Fabien Benetou <fabien-services@benetou.fr>
2015-02-22 11:59:53 +0100

    Unit tests without NotImplemented exceptions

--- tests_for_poem.py
+++ tests_for_poem.py
@@ -16,16 +16,10 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertFalse(words_rhymes("train","potato"))
 
     def test_respect_structure(self):
-        self.structures = {}
-        self.structures['haiku3'] = [(5,None),(7,None),(5,None)]
-        self.structures['rhymetest'] = [(2,1),(2,0)]
+        self.structures = { 'haiku3': [(5, None), (7, None), (5, None)], 'rhymetest' : [(2, 1), (2, 0)] }
         myhaikutest = ['a b c d e','a b c d e f g','a b c d e']
-        #print 'Testing structure respect'
-        #print 'structures:'+ str(structures)
-        #print str(myhaikutest) + ' with structure ' + str(structures['haiku'])
         self.assertTrue(respect_structure(myhaikutest,self.structures['haiku3']))
         myrhymetest = ["patato","potato"]
-        #print str(myrhymetest) + ' with structure ' + str(structures['rhymetest'])
         self.assertTrue(respect_structure(myrhymetest,self.structures['rhymetest']))
 
     def test_get_vocabulary_from_theme(self):
@@ -33,38 +27,51 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertTrue(len(vocabulary) > 0)
 
     def test_get_disambiguation(self):
-        raise NotImplementedError
+        foodrelated = get_disambiguation("I eat on a table.","table")
+        programming_related = get_disambiguation("I program software on a table.","table")
+        self.assertTrue(len(foodrelated)>0)
+        self.assertTrue(len(programming_related)>0)
+        #self.assertTrue(len(get_disambiguation("I like to drink punch.","punch"))>0)
+        #print get_disambiguation("he is playing the bass guitar nicely","bass")
+        #print get_disambiguation("I fish sea bass","bass")
+        #self.assertTrue(foodrelated != programming_related)
 
     def test_find_synonyms(self):
-        # find_synonyms(testwords[0])
-        raise NotImplementedError
+        self.assertTrue("nice" in find_synonyms("good"))
+        self.assertFalse("potato" in find_synonyms("car"))
 
     def test_count_syllables(self):
-        raise NotImplementedError
+        self.assertTrue(count_syllables("table")==2)
+        self.assertFalse(count_syllables("")>0)
 
     def test_syllables_matching_words_from_list(self):
-        # syllables_matching_words_from_list(synonyms,w)
-        raise NotImplementedError
+        self.assertTrue(len(syllables_matching_words_from_list(["healthy","nice"],"very"))==2)
+        self.assertTrue(len(syllables_matching_words_from_list(["healthy","nice"],"potatoes"))==0)
+        self.assertFalse(len(syllables_matching_words_from_list([],"potatoes"))>0)
 
     def test_rhyming_words_from_list(self):
-        # rhyming_words_from_list(synonyms,w)
-        raise NotImplementedError
+        self.assertTrue(len(rhyming_words_from_list(["chorizo","tomato"],"potato"))==1)
+        self.assertTrue(len(rhyming_words_from_list(["chorizo","tomato"],"chair"))==0)
+        self.assertFalse(len(rhyming_words_from_list([],"potatoes"))>0)
 
     def test_spellcheck(self):
-        raise NotImplementedError
+        self.assertTrue(len(spellcheck("This sentence is correct."))==0)
+        self.assertTrue(len(spellcheck("This sentence is incorrtect.")) > 0)
 
     def test_estimate_sentiment(self):
-        raise NotImplementedError
+        [a, b] = estimate_sentiment("I am very happy and this is true.")
+        self.assertTrue(a != 0 and b != 0)
 
     def test_replace_tag(self):
-        raise NotImplementedError
+        sentence1 = 'I want to eat fruits.'
+        self.assertTrue(replace_tag(sentence1,'NNS', 'vegetables')=='I want to eat vegetables.')
+        self.assertTrue(replace_tag(sentence1,'NN', 'vegetables')=='I want to eat fruits.')
 
     def test_respect_grammar(self):
         sentence1 = 'I want to eat fruits.'
         self.assertTrue(respect_grammar(sentence1,['PRP', 'VBP', 'TO', 'VB', 'NNS', '.']))
 
     def ignoreme(self): 
-
         #check coherence : word2vec with pretrained data https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing
         #consider instead cheaper generic NLP : NTLK / spaCy
         generated_paragraph = 'I eat tapas in Spain and camembert in France. They were both delicious but especially the cheese.'
@@ -76,9 +83,6 @@ class TestSequenceFunctions(unittest.TestCase):
         #model.most_similar('dog')
         # consider http://stackoverflow.com/questions/8897593/similarity-between-two-text-documents
         # if uses less memory
-    
-        found_rhyming_words = [] 
-        print 'found_rhyming_words' + str(found_rhyming_words)
 
 if __name__ == '__main__':
     unittest.main()
